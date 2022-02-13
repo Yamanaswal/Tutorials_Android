2328188881aa5acf45250d736e31a7f8803ba5dc
Yamanaswal <yamanaswal@gmail.com>
2022-02-13 13:33:23 +0530

    Progress Dialog Completed

--- app/src/main/java/com/yaman/library_store/MainActivity.kt
+++ app/src/main/java/com/yaman/library_store/MainActivity.kt
@@ -3,14 +3,14 @@ package com.yaman.library_store
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.yaman.progress_dialog.ProgressDialog
+import com.yaman.progress_dialog.ProgressAnimatedDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val progress = ProgressDialog()
+        val progress = ProgressAnimatedDialog()
 
         findViewById<Button>(R.id.openDialog).setOnClickListener {
             progress.show(supportFragmentManager,"asd")
--- progress_dialog/src/main/java/com/yaman/progress_dialog/ProgressAnimatedDialog.kt
+++ progress_dialog/src/main/java/com/yaman/progress_dialog/ProgressAnimatedDialog.kt
@@ -9,34 +9,13 @@ import android.view.ViewGroup
 import android.view.Window
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentManager
-import com.airbnb.lottie.LottieAnimationView
-import com.airbnb.lottie.LottieComposition
-import com.airbnb.lottie.LottieCompositionFactory
-import com.airbnb.lottie.LottieTask
+import com.bumptech.glide.Glide
 import com.yaman.progress_dialog.databinding.ProgressItemBinding
 
-class ProgressDialog() : DialogFragment() {
+
+class ProgressAnimatedDialog() : DialogFragment() {
 
     private lateinit var binding: ProgressItemBinding
-
-    fun constructor()  {
-
-    }
-
-//    private val progressBar: ProgressBar
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    init {
-        println("Init")
-    }
-
-    override fun show(manager: FragmentManager, tag: String?) {
-        super.show(manager, tag)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,23 +28,17 @@ class ProgressDialog() : DialogFragment() {
             container,
             false
         )
-       // Set transparent background and no title
+        // Set transparent background and no title
         if (dialog != null && dialog?.window != null) {
             dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
             dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE);
         }
-        showDialog()
+
+        Glide.with(requireContext()).asGif().load(R.drawable.scooter_anim)
+            .into(binding.orderDeliveryAnim)
+
         return binding.root
     }
 
-    private fun showDialog() {
-        binding.orderDeliveryAnim.speed = 1F
-        binding.orderDeliveryAnim.playAnimation()
-    }
-
-    fun hideDialog() {
-        binding.orderDeliveryAnim.pauseAnimation()
-        dismiss()
-    }
 
 }
