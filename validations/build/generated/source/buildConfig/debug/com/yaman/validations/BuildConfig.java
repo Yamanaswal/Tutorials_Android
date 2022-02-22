01dcac20463c0607e43243c4324fc6f0ad308a96
Yamanaswal <yamanaswal@gmail.com>
2022-02-22 21:31:48 +0530

    Dev Created

---
+++ validations/build/generated/source/buildConfig/debug/com/yaman/validations/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yaman.validations;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.yaman.validations";
+  public static final String BUILD_TYPE = "debug";
+}
