c0e65e2f87d7c9f896df81ba5e12ada586d60c96
Yaman Aswal <yaman.aswal@ripenapps.in>
2022-03-07 12:12:03 +0530

    DatePicker

--- utils/src/main/java/com/yaman/utils/Pickers.kt
+++ utils/src/main/java/com/yaman/utils/Pickers.kt
@@ -1,1 +1,39 @@
 package com.yaman.utils
+
+import android.app.DatePickerDialog
+import android.content.Context
+import android.util.Log
+import android.widget.DatePicker
+import java.util.*
+
+const val TAG = "Pickers"
+
+fun openDatePicker(
+    context: Context,
+    startYear: Int = Calendar.getInstance()[Calendar.YEAR],
+    startMonth: Int = Calendar.getInstance()[Calendar.MONTH],
+    startDay: Int = Calendar.getInstance()[Calendar.DAY_OF_MONTH],
+    minDate: Long = Calendar.getInstance().timeInMillis,
+    isMinDate: Boolean = false,
+    listener: (dayOfMonth:Int, monthOfYear:Int,year:Int) -> Unit
+) {
+
+    //Define Picker
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+            Log.w(TAG, "onDateSet: $year$monthOfYear$dayOfMonth")
+            listener(dayOfMonth, monthOfYear + 1, year)
+        }, startYear, startMonth, startDay
+    )
+
+
+    //No Min Date.
+    if (isMinDate) {
+        datePickerDialog.datePicker.minDate = minDate
+    }
+
+    datePickerDialog.show()
+}
+
+
--- utils/src/main/java/com/yaman/utils/SystemUtils.kt
+++ utils/src/main/java/com/yaman/utils/SystemUtils.kt
@@ -121,3 +121,4 @@ private fun showSettingsDialog(
     }
     builder.show()
 }
+
