41434a858b793cb93eaf02d6afe2feb7433bec07
Yaman Aswal <yaman@appsquadz.com>
2023-10-27 19:27:14 +0530

    Code Updated

--- jetpackpractice/src/main/java/com/yaman/jetpackpractice/ui/pages/LoginPage.kt
+++ jetpackpractice/src/main/java/com/yaman/jetpackpractice/ui/pages/LoginPage.kt
@@ -6,16 +6,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +35,7 @@ fun LoginPage(
 ) {
 
     val context = LocalContext.current
+    var email by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -40,9 +45,8 @@ fun LoginPage(
         LoginAppBar()
         TextField(
             value = "",
-            onValueChange = { text ->
-
-            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { text -> },
             placeholder = { Text("Enter Address") },
             modifier = Modifier
                 .fillMaxWidth()
