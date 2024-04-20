3840475bbb1abd0a083cff4caf389d763d6cdafe
Yaman Aswal <yaman@appsquad.com>
2024-04-20 17:01:36 +0530

    Code

--- dialogs/src/main/java/com/yaman/dialogs/popup_dialogs/DataModels.kt
+++
@@ -1,3 +0,0 @@
-package com.yaman.dialogs.popup_dialogs
-
-data class DialogData(val popUpTitle: String = "PopUp Title.",val popUpDesc: String = "PopUp Description.", val okText : String = "Ok",val cancelText: String = "Cancel")
